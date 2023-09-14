b2390b8a2abbba1c8bc8632cd93680b5a2e608f2
cscaravello <cscaravelloo@gmail.com>
2023-09-14 14:26:50 -0500

    Add project files.

---
+++ TicketingSystemWeek2/InfoManager.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+
+public class InfoManager
+{
+    
+
+    public void Write(string ticketinfo)
+    {
+        StreamWriter sw = new StreamWriter(ticketinfo, true);
+        Console.WriteLine("Enter TicketID:");
+        var ticketID = Console.ReadLine();
+
+        Console.WriteLine("What is the ticket:");
+        var ticketSummary = Console.ReadLine();
+
+        Console.WriteLine("Ticket status:");
+        var ticketStatus = Console.ReadLine();
+
+        Console.WriteLine("Ticket priority:");
+        var ticketPriority = Console.ReadLine();
+
+        Console.WriteLine("Ticket Submitter:");
+        var ticketSubmitter = Console.ReadLine();
+
+        Console.WriteLine("Ticket Assigned:");
+        var ticketAssigned = Console.ReadLine();
+
+        Console.WriteLine("Who is watching");
+        Console.ReadLine();
+        Console.WriteLine("Is anyone else watching? y/n");
+        string selection;
+        do
+        {
+            selection = Console.ReadLine();
+
+            if (selection == "y")
+            {
+                Console.WriteLine("Who is watching");
+                Console.ReadLine();
+            }
+            if (selection == "n")
+            {
+                break;
+            }
+        } while (selection != "n");
+
+
+        sw.Close();
+}
+    public void Read(string ticketinfo)
+    {
+        if (File.Exists(ticketinfo))
+        {
+            StreamReader sr = new StreamReader(ticketinfo);
+            Console.WriteLine(Write);
+
+           
+
+
+        }
+    }
+}
---
+++ TicketingSystemWeek2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class TicketingSystem
+{
+    public static void Main()
+    {
+        InfoManager infoManager = new InfoManager();
+
+        string ticketinfo = "ticketinfo.txt";
+        string menu;
+
+      
+
+
+
+                do
+        {
+            Console.WriteLine("1. Edit File");
+            Console.WriteLine("2. Read File");
+            Console.WriteLine("3. Exit System");
+            menu = Console.ReadLine();
+
+            if (menu == "1")
+            {
+                infoManager.Write(ticketinfo);
+
+            }
+            else if (menu == "2")
+            {
+                infoManager.Read(ticketinfo);
+
+            }
+            else if (menu == "3")
+            {
+
+            }
+
+
+        } while (menu != "3");
+
+    }
+}
+
